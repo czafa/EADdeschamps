44e5af34912035e828a10fdcbad64bceed5dc932
Echo <caiozafa25@gmail.com>
2025-02-14 13:59:23 +0000

    returning

---
+++ infra/database.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+async function query(queryObject) {
+  const client = new Client();
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
+import database from "../../../../infra/database.js";
+
 function status(request, response) {
+  console.log(database);
   response.status(200).json({ status: "testando server local" });
 }
 
